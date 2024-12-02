32422287923bb4c6a68aa24cf6cab196a050bb21
Tuna Arikaya <155156717+tunaarikaya@users.noreply.github.com>
2024-12-02 17:41:24 +0300

    Update CircularShape.swift

--- CircularSwiftUI/CircularSwiftUI/CircularShape.swift
+++ CircularSwiftUI/CircularSwiftUI/CircularShape.swift
@@ -15,7 +15,7 @@ struct CircularShape: Shape {
     var animatableData: Double {
         get {
             return percent
-            //733
+            //7332
         }
         set{
             percent = newValue
